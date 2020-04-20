a90fb1e2c42bdaa03258f8e8cf6ea2be46257bc3
rajmahal4 <rsingh6@mail.sfsu.edu>
2020-04-19 19:28:39 -0700

    first commit

---
+++ src/homeTab.js
@@ -0,0 +1,6 @@
+const homeTab = (container) => {
+    const navbar = document.createElement('navbar')
+    navbar.classList.add("nav");
+    container.appendChild(navbar);
+}
+export default homeTab;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+import homeTab from './homeTab';
+
+const container = document.querySelector('#content');
+const head = document.querySelector('#head');
+
+homeTab(container);
