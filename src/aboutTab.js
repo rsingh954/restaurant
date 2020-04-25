fda768a9e3fad6927c775b0f1e483d48b3b50960
rajmahal4 <rsingh6@mail.sfsu.edu>
2020-04-24 21:03:42 -0700

    final versionv1

--- src/aboutTab.js
+++ src/aboutTab.js
@@ -7,7 +7,7 @@ const about = () =>{
     <div class="cardText">
       <h4> About | Roma's Deli </h4>
       <hr>
-      <p><em>Roma's has served San Bruno for years. Our customers have have grown befire our eyes. We are a staple in 
+      <p><em>Roma's has served San Bruno for years. Our customers have have grown before our eyes. We are a staple in 
       the community.</p>
     </div>
   </div>
